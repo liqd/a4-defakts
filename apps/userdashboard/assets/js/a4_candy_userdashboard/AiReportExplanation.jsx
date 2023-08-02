86ccb440d195fbb8754d2c812f13d1dd04d4736f
philli-m <5871230+philli-m@users.noreply.github.com>
2023-08-02 10:39:17 +0200

    userdashboard/AiReportExplanation: add switch component to display/hide ai explanation on frontend with test and update snapshot

--- apps/userdashboard/assets/js/a4_candy_userdashboard/AiReportExplanation.jsx
+++ apps/userdashboard/assets/js/a4_candy_userdashboard/AiReportExplanation.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react'
 import django from 'django'
+import { SwitchButton } from '../../../../../apps/contrib/assets/SwitchButton'
 
 const translated = {
-  intro: django.gettext('This comment contains disinformation. Defakts uses an Artificial ' +
+  intro: django.pgettext('defakts', 'This comment contains disinformation. Defakts uses an Artificial ' +
   'Intelligence to scan content for disinformation. Disinformation often shows ' +
   'certain characteristics that allow for a reliable identification.'),
-  ariaReadMore: django.gettext('Click to view the AI explanation for reporting this comment.'),
-  ariaReadLess: django.gettext('Click to hide the AI explanation for reporting this comment.'),
-  readMore: django.gettext('Read more'),
-  readLess: django.gettext('Read less')
+  ariaReadMore: django.pgettext('defakts', 'Click to view the AI explanation for reporting this comment.'),
+  ariaReadLess: django.pgettext('defakts', 'Click to hide the AI explanation for reporting this comment.'),
+  readMore: django.pgettext('defakts', 'Read more'),
+  readLess: django.pgettext('defakts', 'Read less'),
+  showInfoSwitch: django.pgettext('defakts', 'Show info to users'),
+  hideInfoSwitch: django.pgettext('defakts', 'Hide info from users')
 }
 
-export const AiReportExplanation = ({ AiReport }) => {
+export const AiReportExplanation = ({ AiReport, notificationPk }) => {
   const [isExpanded, setIsExpanded] = useState()
 
   const toggleExpand = () => {
@@ -19,23 +22,32 @@ export const AiReportExplanation = ({ AiReport }) => {
   }
 
   return (
-    <li className="alert alert--danger d-flex text-start fs-6 mb-3">
-      <i
-        className="fas fa-exclamation-circle text-danger pt-1 pe-2"
-        aria-hidden="True"
-      />
-      <p className="pe-4">
-        {translated.intro + ' '}
-        {isExpanded && AiReport + ' '}
-        <button
-          className="btn--link text-danger"
-          type="button"
-          onClick={toggleExpand}
-          aria-label={isExpanded ? translated.ariaReadLess : translated.ariaReadMore}
-        >
-          {isExpanded ? translated.readLess : translated.readMore}
-        </button>
-      </p>
+    <li className="alert alert--danger mb-4">
+      <div className="d-flex text-start mb-4">
+        <i
+          className="fas fa-exclamation-circle text-danger pt-1 pe-2"
+          aria-hidden="True"
+        />
+        <p className="pe-4">
+          {translated.intro + ' '}
+          {isExpanded && AiReport + ' '}
+          <button
+            className="btn--link text-danger"
+            type="button"
+            onClick={toggleExpand}
+            aria-label={isExpanded ? translated.ariaReadLess : translated.ariaReadMore}
+          >
+            {isExpanded ? translated.readLess : translated.readMore}
+          </button>
+        </p>
+      </div>
+      <div className="d-flex text-start">
+        <SwitchButton
+          id={notificationPk}
+          switchLabelOn={translated.hideInfoSwitch}
+          switchLabelOff={translated.showInfoSwitch}
+        />
+      </div>
     </li>
   )
 }
