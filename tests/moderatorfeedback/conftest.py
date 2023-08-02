d121bb6b4307c7066b0b39e63020e206e8050565
Mara Karagianni <m.karagianni@liqd.net>
2023-08-02 14:26:55 +0300

    apps/moderatorfeedback: add ai_report serialiser

--- tests/moderatorfeedback/conftest.py
+++ tests/moderatorfeedback/conftest.py
@@ -1,8 +1,10 @@
 from pytest_factoryboy import register
 
+from tests.ai_reports.factories import AiReportFactory
 from tests.ideas.factories import IdeaFactory
 
 from .factories import ModeratorCommentFeedbackFactory
 
+register(AiReportFactory)
 register(IdeaFactory)
 register(ModeratorCommentFeedbackFactory)
