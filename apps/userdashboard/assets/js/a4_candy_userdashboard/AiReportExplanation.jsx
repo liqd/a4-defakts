3c9782a537d83666c2837d09bbe4eddfc8e53b23
philli-m <5871230+philli-m@users.noreply.github.com>
2023-08-16 16:46:46 +0200

    userdashboard: ensure aiexplanation switch always on and replace dummy text with real text from ai report

--- apps/userdashboard/assets/js/a4_candy_userdashboard/AiReportExplanation.jsx
+++ apps/userdashboard/assets/js/a4_candy_userdashboard/AiReportExplanation.jsx
@@ -9,7 +9,7 @@ const translated = {
   ariaReadMore: django.pgettext('defakts', 'Click to view the AI explanation for reporting this comment.'),
   ariaReadLess: django.pgettext('defakts', 'Click to hide the AI explanation for reporting this comment.'),
   readMore: django.pgettext('defakts', 'Read more'),
-  readLess: django.pgettext('defakts', 'Read less'),
+  showLess: django.pgettext('defakts', 'Show less'),
   showInfoSwitch: django.pgettext('defakts', 'Show info to users'),
   hideInfoSwitch: django.pgettext('defakts', 'Hide info from users')
 }
@@ -21,6 +21,17 @@ export const AiReportExplanation = ({ AiReport, notificationPk }) => {
     setIsExpanded(!isExpanded)
   }
 
+  const toggleReadMore = (
+    <button
+      className="btn--link text-danger"
+      type="button"
+      onClick={toggleExpand}
+      aria-label={isExpanded ? translated.ariaReadLess : translated.ariaReadMore}
+    >
+      {isExpanded ? translated.showLess : translated.readMore}
+    </button>
+  )
+
   return (
     <li className="alert alert--danger mb-4">
       <div className="d-flex text-start mb-4">
@@ -28,21 +39,21 @@ export const AiReportExplanation = ({ AiReport, notificationPk }) => {
           className="fas fa-exclamation-circle text-danger pt-1 pe-2"
           aria-hidden="True"
         />
-        <p className="pe-4">
-          {translated.intro + ' '}
-          {isExpanded && AiReport + ' '}
-          <button
-            className="btn--link text-danger"
-            type="button"
-            onClick={toggleExpand}
-            aria-label={isExpanded ? translated.ariaReadLess : translated.ariaReadMore}
-          >
-            {isExpanded ? translated.readLess : translated.readMore}
-          </button>
-        </p>
+
+        {!isExpanded
+          ? (
+            <p className="pe-4">{translated.intro} {toggleReadMore}</p>
+            )
+          : (
+            <div className="pe-4">
+              <p>{translated.intro}</p>
+              <p>{AiReport} {toggleReadMore}</p>
+            </div>
+            )}
       </div>
       <div className="d-flex text-start">
         <SwitchButton
+          initiallyChecked
           id={notificationPk}
           switchLabelOn={translated.hideInfoSwitch}
           switchLabelOff={translated.showInfoSwitch}
