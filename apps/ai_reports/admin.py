61e2c546dd905eb5f114214c3b412e2341f7180f
Mara Karagianni <m.karagianni@liqd.net>
2023-08-16 15:16:54 +0300

    apps/ai_reports: fix admin comment relation and display

--- apps/ai_reports/admin.py
+++ apps/ai_reports/admin.py
@@ -6,10 +6,30 @@ from .models import AiReport
 @admin.register(AiReport)
 class AiReportAdmin(admin.ModelAdmin):
     model = AiReport
-    fields = [
+    list_display = (
+        "__str__",
+        "category",
+        "confidence",
+        "is_pending",
+        "comment",
+    )
+    list_filter = (
+        "is_pending",
+        "category",
+        "confidence",
+        "comment",
+    )
+    search_fields = (
+        "category",
+        "explanation",
+        "confidence",
+        "is_pending",
+    )
+    fields = (
         "is_pending",
         "category",
         "explanation",
         "confidence",
-    ]
+        "comment",
+    )
     raw_id_fields = ("comment",)
