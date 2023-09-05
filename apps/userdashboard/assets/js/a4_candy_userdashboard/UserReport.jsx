723e8243e4986111e7cecb2087aa065d2747a365
Julian Dehm <j.dehm@liqd.net>
2023-09-05 16:56:34 +0200

    apps/userdashboard: show real user reports in dashboard and update styling

--- apps/userdashboard/assets/js/a4_candy_userdashboard/UserReport.jsx
+++ apps/userdashboard/assets/js/a4_candy_userdashboard/UserReport.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import django from 'django'
 import Slider from 'react-slick'
+
+const translated = {
+  userReport: django.pgettext('defakts', 'User {}username{} reported the comment and gave this reason:')
+}
 
 export const UserReport = (props) => {
   const settings = {
@@ -13,6 +18,17 @@ export const UserReport = (props) => {
     centerPadding: '0px'
   }
 
+  function getUsername (string, userName) {
+    const splitted = string.split('{}')
+    return (
+      <p>
+        {splitted[0]}
+        <b>{splitted[1]}</b>
+        {splitted[2]}
+      </p>
+    )
+  }
+
   return (
     <li className="alert alert--danger pb-0">
       <Slider {...settings}>
@@ -22,9 +38,10 @@ export const UserReport = (props) => {
             data-index={index}
             key={index}
           >
-            <p className="poll-slider__answer">
-              {slide}
-            </p>
+            <div className="poll-slider__answer">
+              {getUsername(translated.userReport, slide.username)}
+              <p className={'fs-6 ' + (props.userReport.length > 1 ? '' : 'mb-0')}>"{slide.description}"</p>
+            </div>
             <div className={props.userReport.length > 1 ? 'poll-slider__count--spaced' : 'poll-slider__count'}>
               {index + 1}/{props.userReport.length}
             </div>
